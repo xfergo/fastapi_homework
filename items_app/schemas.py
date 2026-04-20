8eba83cbc59e92eff9ea16b2a94d835a68264e93
xfergo <igorboshkov@gmail.com>
2026-04-20 14:51:50 +0200

    name field rework

--- items_app/schemas.py
+++ items_app/schemas.py
@@ -9,7 +9,7 @@ class ItemCreate(BaseModel):
 
 class ItemResponse(BaseModel):
     id: int
-    name: Annotated[str, Field(exclude=True, examples=["dsafasdf"])]
+    name: Annotated[str, Field(examples=["dsafasdf"])]
     price: Annotated[float, Field(examples=[12,2])]
     in_stock: Annotated[bool, Field(examples=["true"])]
     model_config = {"from_attributes": True}
